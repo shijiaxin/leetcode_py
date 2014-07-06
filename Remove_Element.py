367efcd7ca015dfffa72fadba298664fd4e2ac3e
shijiaxin <shijiaxin.cn@gmail.com>
2014-07-06 23:27:51 +0800

    Remove Element

--- Remove_Element.py
+++ Remove_Element.py
@@ -3,3 +3,12 @@ class Solution:
     # @param    elem    an integer, value need to be removed
     # @return an integer
     def removeElement(self, A, elem):
+    	l=len(A);
+    	pos=0;
+    	while(pos<l):
+    		if(A[pos]==elem):
+    			A[pos],A[l-1]=A[l-1],A[pos];
+    			l=l-1;
+    		else:
+    			pos=pos+1;
+    	return l;
