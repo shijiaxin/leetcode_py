0adb6ab052e5385367be4ca9853e8f0ca4c16859
shijiaxin <shijiaxin.cn@gmail.com>
2014-07-10 10:11:30 +0800

    modified Roman to Integer

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,30 +1,22 @@
 class Solution:
-	def smallToRoman(self,str,num):
-		if (num <= 3):
-			return str[0]*num;
-		if (num == 4):
-			return str[0]+str[1];
-		if (num <= 8):
-			return str[1]+str[0]*(num-5);
-		return str[0]+str[2];
+	def charToInt(self,s):
+		arr="IVXLCDM";
+		intarr=[1,5,10,50,100,500,1000];
+		pos=arr.find(s);
+		if(pos>=0 & pos<len(intarr)):
+			return intarr[pos];
+		return 0;
 	# @return an integer
 	def romanToInt(self, s):
 		arr=["IVX","XLC","CDM","M??"];
 		result=0;
-		# first handle number more than 1000
-		for i in range(3,0,-1):
-			str=self.smallToRoman(arr[3],i);
-			if(s[0:len(str)]==str):
-				s=s[len(str):];
-				result+=i*1000;
-				break;
-		for k in range(2,-1,-1):
-			for i in range(9,0,-1):
-				str=self.smallToRoman(arr[k],i);
-				if(s[0:len(str)]==str):
-					s=s[len(str):];
-					result+=i*(10**k);
-					break;
+		last=0;
+		for c in s:
+			r=self.charToInt(c);
+			if(r==(last*10) or r==(last*5)):
+				result-=2*last;
+			result+=r;
+			last=r;
 		return result;
 s=Solution();
 print s.romanToInt("XCVIII");
